cc0e741dd201f8b44a63dd0de9d923275b5aa9ea
Badjemond1328 <mking56705@gmail.com>
2026-02-07 18:39:14 -0500

    first commit

---
+++ hello/target/debug/deps/hello-7e7ac79519dc622d.d
@@ -0,0 +1,5 @@
+/home/bjaschool/Documents/YWCC207/hello/target/debug/deps/hello-7e7ac79519dc622d.d: src/main.rs
+
+/home/bjaschool/Documents/YWCC207/hello/target/debug/deps/hello-7e7ac79519dc622d: src/main.rs
+
+src/main.rs:
---
+++ hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/home/bjaschool/Documents/YWCC207/hello/target/debug/hello: /home/bjaschool/Documents/YWCC207/hello/src/main.rs
